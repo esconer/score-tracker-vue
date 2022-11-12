bf17f22a480369c7cc5ff7e65df9ab38526457f9
esconer <esconer@proton.me>
2022-11-12 08:51:15 +0530

    reset

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,17 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script lang="ts">
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<p>Hello World</p>
+</template> 
 
 <style scoped>
-header {
-  line-height: 1.5;
+:root{
+  font-size: 12px;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+p{
+  text-align: center;
+  font-size: 3em;
 }
 </style>
