bcf1f2f2c033191d47a1617fbbfdf8f51284e4ef
Sumit-Dey-2002 <sumitdeypapan2002@gmail.com>
2022-11-12 09:30:18 +0530

    tailwind init

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,5 @@
 </script>
 
 <template>
-<p>Hello World</p>
+<p class="text-lg text-red-600">Hello World</p>
 </template> 
-
-<style scoped>
-:root{
-  font-size: 12px;
-}
-p{
-  text-align: center;
-  font-size: 3em;
-}
-</style>
