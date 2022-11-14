fd8c3f5946f52fa67414d86192987f7105e94213
Shirsendu Bairagi <shirsendu2001@gmail.com>
2022-11-14 20:23:18 +0530

    chores: todos

--- src/components/PlayerCard.vue
+++ src/components/PlayerCard.vue
@@ -2,59 +2,75 @@
 import Circle from '@/assets/icons/circle.svg';
 import Cross from '@/assets/icons/cross.svg';
 import Right from '@/assets/icons/right.svg';
-import Dots from '@/assets/icons/sots.svg';
+import Dots from '@/assets/icons/dots.svg';
 </script>
 
 <template>
 	<!-- NOTE: use the imported Circle, Cross etc int the :src of a image tag -->
 	<!-- <img :src="Circle" alt="circle" /> -->
-<body>
-    <div class="container1">
-        <div>
-            <p id="Header">Total player 4</p>
-			<br>
-            <span class="container2">
-				<p id="Player-num">#1</p>
-                <p id="Player-name">Sumit Dey</p>
-            </span>
-			<p>Success Rate 79%</p>
-			<p>Error Rate 15%</p>
-			<p>Skip Rate 6%</p>
-			<p id="Score">Score 98.2</p>
-        </div>
-        <div>
-            <p id="Header">Total Questions 129</p>
-            <br>
-			<span class="container2">
-				<div>
-					<img :src="Right" alt="right" id="Right">
-					102
-				</div>
-				<div>
-					<img :src="Cross" alt="cross" id="Cross">
-					19
-				</div>
-				<div>
-					<img :src="Circle" alt="circle" id="Circle">
-					8
-				</div>
-            </span>
-			<div id="Option">	
-			<img :src="Dots" alt="dots">
+
+	<!-- TODO: set default formatter volar -->
+	<!-- TODO: setup android devices view on browser for pixel perfect development -->
+
+	<!-- TODO: comment out any code outside of the card -->
+	<!-- TODO: ids are not needed in a javascript framework so remove it -->
+	<!-- TODO: leave margin and padding -->
+	<!-- TODO: decrease font size -->
+	<!-- NOTE: use tailwind css for better DX -->
+	<!-- TODO: body is used only once in a html so never use body in component -->
+
+	<body>
+		<div class="container1">
+			<div>
+				<p id="Header">Total player 4</p>
+				<br>
+				<span class="container2">
+					<p id="Player-num">#1</p>
+					<p id="Player-name">Sumit Dey</p>
+				</span>
+				<p>Success Rate 79%</p>
+				<p>Error Rate 15%</p>
+				<p>Skip Rate 6%</p>
+				<p id="Score">Score 98.2</p>
 			</div>
-        </div>
-    </div>
-</body>
+			<div>
+				<p id="Header">Total Questions 129</p>
+				<br>
+				<!-- TODO: use grid for better layout -->
+				<span class="container2">
+					<div>
+						<img :src="Right" alt="right" id="Right">
+						102
+					</div>
+					<div>
+						<img :src="Cross" alt="cross" id="Cross">
+						19
+					</div>
+					<div>
+						<img :src="Circle" alt="circle" id="Circle">
+						8
+					</div>
+				</span>
+				<div id="Option">
+					<!-- TODO: use absolute positioning for the dot -->
+					<img :src="Dots" alt="dots">
+				</div>
+			</div>
+		</div>
+	</body>
 </template>
 
 <style>
 .container {
 	display: flex;
+	/* TODO: spa is a invalid value for justify-content*/
+	/* TODO: Invoke intellisense for getting possible valid values */
 	justify-content: spa;
 }
+
 .container1 {
-    display: flex;
-    justify-content: space-around;
+	display: flex;
+	justify-content: space-around;
 	border-radius: 5%;
 	background-color: rgb(199, 223, 246);
 }
@@ -95,6 +111,7 @@ P {
 	font-size: xx-large;
 }
 
+/* TODO: use hex code instead of rgb */
 #Right {
 	background-color: green;
 	border-radius: 20%;
