2a2c8d0f047fdb1a71fe8fa7e8e6a9eb8bafcfcb
Sumit-Dey-2002 <sumitdeypapan2002@gmail.com>
2022-11-27 17:55:42 +0530

    playercard modified

--- src/components/PlayerCard.vue
+++ src/components/PlayerCard.vue
@@ -3,125 +3,62 @@ import Circle from '@/assets/icons/circle.svg';
 import Cross from '@/assets/icons/cross.svg';
 import Right from '@/assets/icons/right.svg';
 import Dots from '@/assets/icons/dots.svg';
+import { computed, ref } from 'vue';
+
+const props = defineProps({
+	rank: { type: Number, required: true },
+	playerName: { type: String, required: true },
+	totalCount: { type: Number, required: true },
+	rightCount: { type: Number, required: true },
+	wrongCount: { type: Number, required: true }
+})
+
+/* const rank = ref(10) */
+/* const playerName = ref("I'm esconer") */
+/* const totalCount = ref(150) */
+/* const rightCount = ref(70) */
+/* const wrongCount = ref(70) */
+
+const attempted = computed(() => props.rightCount + props.wrongCount)
+const unattempted = computed(() => props.totalCount - attempted.value)
+const successRate = computed(() => props.rightCount / attempted.value * 100)
+const errorRate = computed(() => props.wrongCount / attempted.value * 100)
+const skipRate = computed(() => (props.totalCount - attempted.value) / props.totalCount * 100)
+const score = computed(() => props.rightCount * 1 - props.wrongCount * .25)
 </script>
 
 <template>
-	<!-- NOTE: use the imported Circle, Cross etc int the :src of a image tag -->
-	<!-- <img :src="Circle" alt="circle" /> -->
-
-	<!-- TODO: set default formatter volar -->
-	<!-- TODO: setup android devices view on browser for pixel perfect development -->
-
-	<!-- TODO: comment out any code outside of the card -->
-	<!-- TODO: ids are not needed in a javascript framework so remove it -->
-	<!-- TODO: leave margin and padding -->
-	<!-- TODO: decrease font size -->
-	<!-- NOTE: use tailwind css for better DX -->
-	<!-- TODO: body is used only once in a html so never use body in component -->
-
-	<body>
-		<div class="container1">
+	<div class="flex">
+		<ul class="grid">
+			<h1><span>#1</span><span>Sumit Dey</span></h1>
+			<span>Success Rate 79%</span>
+			<span>Error Rate 15%</span>
+			<span>Skip Rate 6%</span>
+			<Span>Score 98.2</Span>
+		</ul>
+		<div class="grid">
 			<div>
-				<span class="container2">
-					<p id="Player-num">#1</p>
-					<p id="Player-name">Sumit Dey</p>
-				</span>
-				<p>Success Rate 79%</p>
-				<p>Error Rate 15%</p>
-				<p>Skip Rate 6%</p>
-				<p id="Score">Score 98.2</p>
+				<img :src="Right" alt="Right">
+				<h1>102</h1>
 			</div>
 			<div>
-				<!-- TODO: use grid for better layout -->
-				<span class="container2">
-					<div>
-						<img :src="Right" alt="right" id="Right">
-						102
-					</div>
-					<div>
-						<img :src="Cross" alt="cross" id="Cross">
-						19
-					</div>
-					<div>
-						<img :src="Circle" alt="circle" id="Circle">
-						8
-					</div>
-					<div class="flex w-4 h-4 bg-[#1e6ee5]">
-					</div>
-				</span>
-				<div id="Option">
-					<!-- TODO: use absolute positioning for the dot -->
-					<img :src="Dots" alt="dots">
-				</div>
+				<img :src="Cross" alt="Cross">
+				<h1>19</h1>
+			</div>
+			<div>
+				<img :src="Circle" alt="Circle">
+				<h1>8</h1>
+			</div>
+			<div>
+				<img :src="Dots" alt="Dots">
 			</div>
 		</div>
-	</body>
+	</div>
 </template>
 
 <style>
-.container {
-	display: flex;
-	/* TODO: spa is a invalid value for justify-content*/
-	/* TODO: Invoke intellisense for getting possible valid values */
-	justify-content: space-around;
-}
-
-.container1 {
-	display: flex;
-	justify-content: space-around;
+div {
+	background-color: #E3EEFF;
 	border-radius: 5%;
-	background-color: rgb(199, 223, 246);
-}
-
-.container2 {
-	display: flex;
-	justify-content: space-between;
-	text-align: center;
-}
-
-#Header {
-	/* font-weight: bold; */
-	font-size: x-large;
-	text-align: center;
-}
-
-P {
-	text-align: left;
-}
-
-#Player-num {
-	color: rgb(188, 185, 185);
-	text-align: left;
-	font-size: xx-large
-}
-
-#Player-name {
-	/* font-weight: bold; */
-	font-size: xx-large;
-}
-
-#Option {
-	text-align: right;
-}
-
-#Score {
-	font-weight: bold;
-	font-size: xx-large;
-}
-
-/* TODO: use hex code instead of rgb */
-#Right {
-	background-color: green;
-	border-radius: 20%;
-}
-
-#Cross {
-	background-color: rgb(183, 3, 102);
-	border-radius: 20%;
-}
-
-#Circle {
-	background-color: rgb(24, 87, 248);
-	border-radius: 20%;
 }
 </style>
